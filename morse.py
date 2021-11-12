ae7d307f2fd8cf1a3453d0a5bc32ab96081fc6e8
mohammad <mr.dori.dev@gmail.com>
2021-11-12 22:53:25 +0330

    make code stacture

--- morse.py
+++ morse.py
@@ -2,6 +2,27 @@
 and play morse code sound.
 """
 
+# Standard Library import
 from time import sleep
+
+# Third party import
 from vlc import MediaPlayer
 
+# Local import
+from assets import morse_rules
+
+
+def play_sound(characters: "str"):
+    pass
+
+
+text = input("Text: ").upper()
+morse_code = ""
+
+for letter in text:
+    pass
+
+play_sound(morse_code)
+
+morse_code_style = morse_code.replace(".", "•").replace("-", "−")
+print(f"{text} : {morse_code_style}")
