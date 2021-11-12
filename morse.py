72ddf869923da590c56b0ca7a04fbe067d07cc24
mohammad <mr.dori.dev@gmail.com>
2021-11-12 22:59:43 +0330

    comlite code

--- morse.py
+++ morse.py
@@ -16,7 +16,7 @@ def play_sound(time_left: int):
     song = MediaPlayer('beep.mp3')
     song.play()
     song.set_time(time_left)
-    sleep(0.3)
+    sleep(0.2)
 
 
 def play_morse(characters: "str"):
@@ -27,16 +27,17 @@ def play_morse(characters: "str"):
             play_sound(800)
         else:
             sleep(2)
+    sleep(0.5)
 
 
 text = input("Text: ").upper()
-morse_code = ""
+morse_codes = ""
 
 for letter in text:
-    pass
+    morse_codes += morse_rules.get(letter, " ")
 
-play_morse(morse_code)
+play_morse(morse_codes)
 
-morse_code_style = morse_code.replace(
+morse_code_style = morse_codes.replace(
     ".", "•").replace("-", "−").replace(" ", " |  ")
 print(f"{text} : {morse_code_style}")
